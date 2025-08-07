c0946166b5feccba452dddf343b13b3382c4c2f1
SD-Media <sd-media@t-online.de>
2025-08-07 19:25:27 +0200

    Delete main.js

--- main.js
+++
@@ -1,1 +0,0 @@
-function showTab(id) { document.querySelectorAll('.tab-content').forEach(e => e.classList.remove('active')); document.getElementById(id).classList.add('active'); document.querySelectorAll('.nav-tab').forEach(e => e.classList.remove('active')); event.target.classList.add('active'); }
